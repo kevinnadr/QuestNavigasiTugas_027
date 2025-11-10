5d062a8af1aaea03349d2fcd9eaa3f3b10f531af
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 07:44:38 +0700

    Membuat kolom dan mengatur jaraknya

--- app/src/main/java/com/example/questnavigasitugas_027/Views/Welcome.kt
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/Welcome.kt
@@ -1,11 +1,15 @@
 package com.example.questnavigasitugas_027.Views
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasitugas_027.R
 
 @Composable
@@ -18,4 +22,11 @@ fun WelcomeScreen(
         contentDescription = null,
         contentScale = ContentScale.Crop
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 140.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){}
 }

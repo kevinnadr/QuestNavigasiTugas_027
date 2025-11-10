c8df0601f69612eaaec5ba8d79708fb318b6fd63
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 09:00:10 +0700

    Membuat bottombar untuk button beranda dan formulir dan memberikan text yang diambil dari stringresources

--- app/src/main/java/com/example/questnavigasitugas_027/Views/List.kt
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/List.kt
@@ -1,6 +1,11 @@
 package com.example.questnavigasitugas_027.Views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
@@ -10,10 +15,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.questnavigasitugas_027.Data
+import com.example.questnavigasitugas_027.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,5 +47,22 @@ fun ListScreen(
                 )
             )
         },
-    ) {  }
+        bottomBar ={
+            BottomAppBar(
+                containerColor = Color.Transparent,
+            ) {
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    Button(onClick = onBrndBtnClick) {
+                        Text(text = stringResource(id = R.string.list_beranda))
+                    }
+                    Button(onClick = onFormsBtnClick) {
+                        Text(text = stringResource(id = R.string.list_formulir))
+                    }
+                }
+            }
+        }
+    ) {}
 }

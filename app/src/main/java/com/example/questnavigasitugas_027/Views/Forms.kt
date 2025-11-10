f924b286e2cba6a4f72d981a1255d04f255bb587
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 10:48:28 +0700

    Membuat file Forms.kt dan fun Formscreen

---
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/Forms.kt
@@ -0,0 +1,13 @@
+package com.example.questnavigasitugas_027.Views
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.questnavigasitugas_027.Data
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormScreen(
+    onSmbtBtnClick: (Data) -> Unit,
+    onKmblBtnClick: () -> Unit
+) {}

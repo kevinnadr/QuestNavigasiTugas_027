9af13c3d924a740cb02887a5b91a38ec74e572b8
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 09:29:32 +0700

    menambahkan column dan lazycolumn untuk fitur scrolling

--- app/src/main/java/com/example/questnavigasitugas_027/Views/List.kt
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/List.kt
@@ -1,11 +1,15 @@
 package com.example.questnavigasitugas_027.Views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -18,10 +22,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigasitugas_027.Data
 import com.example.questnavigasitugas_027.R
 
@@ -38,10 +46,11 @@ fun ListScreen(
                 title = {
                     Text(
                         text = "LIST DATA DIRI",
-                        modifier = Modifier.padding(start = 107.dp),
+                        modifier = Modifier.padding(start = 77.dp),
+                        fontSize = 30.sp,
                         fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center,
-                        color = Color.Black,
+                        color = Color.LightGray,
                         style = MaterialTheme.typography.headlineSmall
                     )
                 },
@@ -71,7 +80,28 @@ fun ListScreen(
         Box(
             modifier = Modifier.fillMaxSize()
         ){
-
+            Image(
+                painter = painterResource(id = R.drawable.black),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                alpha = 1f
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(start = 15.dp, end = 15.dp)
+            ) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+                ) {
+                    items(listData) { data ->
+                        dataCard(data = data)
+                    }
+                }
+            }
         }
     }
 }

1700d64d076555263e2f6020d5775063ffb31924
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 07:42:42 +0700

    menambahkan onMskBtnClick dan menambahkan foto warna untuk background

--- app/src/main/java/com/example/questnavigasitugas_027/Views/Welcome.kt
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/Welcome.kt
@@ -1,6 +1,21 @@
 package com.example.questnavigasitugas_027.Views
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.questnavigasitugas_027.R
 
 @Composable
-fun WelcomeScreen(){}
+fun WelcomeScreen(
+    onMskBtnClick: () -> Unit
+){
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        painter = painterResource(id = R.drawable.redcanvas),
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
+}

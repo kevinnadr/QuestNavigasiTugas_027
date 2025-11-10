b6e9090bf0c050435befeb5732441bef4880a6eb
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 08:48:09 +0700

    Membuat file List.kt dan membuat fun list screen

---
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/List.kt
@@ -0,0 +1,13 @@
+package com.example.questnavigasitugas_027.Views
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.questnavigasitugas_027.Data
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListScreen(
+    listData: List<Data>,
+    onBrndBtnClick: () -> Unit,
+    onFormsBtnClick: () -> Unit
+){}

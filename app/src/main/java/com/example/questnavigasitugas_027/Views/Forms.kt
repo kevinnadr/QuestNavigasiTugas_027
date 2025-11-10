cb575bb6e0169bce0a706c112ce4e9537f373582
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 13:50:53 +0700

    membuat column dan text formulir pendaftaran

--- app/src/main/java/com/example/questnavigasitugas_027/Views/Forms.kt
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/Forms.kt
@@ -1,17 +1,28 @@
 package com.example.questnavigasitugas_027.Views
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.example.questnavigasitugas_027.Data
 import com.example.questnavigasitugas_027.R
 
@@ -53,4 +64,22 @@ fun FormScreen(
         contentDescription = null,
         contentScale = ContentScale.Crop
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = stringResource(id = R.string.form_judul),
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.LightGray,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium))
+        )
+    }
 }

83a5d99234d91c8ddd0c9fa5420e49e06a3c682c
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-10 07:51:46 +0700

    Menambahkan text selamat datang lalu mengatur jarak textnya dan jarak antara text dan logo/gambar

--- app/src/main/java/com/example/questnavigasitugas_027/Views/Welcome.kt
+++ app/src/main/java/com/example/questnavigasitugas_027/Views/Welcome.kt
@@ -2,14 +2,20 @@ package com.example.questnavigasitugas_027.Views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigasitugas_027.R
 
 @Composable
@@ -28,5 +34,14 @@ fun WelcomeScreen(
             .fillMaxSize()
             .padding(top = 140.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){}
+    ){
+        Text(
+            text = "Selamat Datang",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.Gray,
+        )
+        Spacer(modifier = Modifier.padding(top = 50.dp))
+    }
 }
